693052b1978ab6b2958207ffb05c615fa3fb97b0
Zac Tressel <tress018@umn.edu>
2020-02-06 15:07:40 -0600

    Started Cryptology Project

---
+++ CryptologyGame/src/ConverterWindow.java
@@ -0,0 +1,135 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class ConverterWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ConverterWindow window = new ConverterWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ConverterWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 990, 639);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLACK);
+		panel.setBounds(0, 0, 968, 583);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JCheckBox phiCheckbox = new JCheckBox("Phi Function");
+		phiCheckbox.setBounds(271, 282, 196, 29);
+		panel.add(phiCheckbox);
+		
+		JLabel lblNewLabel = new JLabel("Choose Your Problems");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 60));
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBounds(112, 57, 761, 86);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Type");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		lblNewLabel_1.setForeground(Color.WHITE);
+		lblNewLabel_1.setBounds(157, 206, 139, 35);
+		panel.add(lblNewLabel_1);
+		
+		JCheckBox shiftCheckbox = new JCheckBox("Shift Cipher");
+		shiftCheckbox.setBounds(40, 282, 161, 29);
+		panel.add(shiftCheckbox);
+		
+		JCheckBox affineCheckbox = new JCheckBox("Affine Cipher");
+		affineCheckbox.setBounds(40, 319, 161, 29);
+		panel.add(affineCheckbox);
+		
+		JCheckBox vigenereCheckbox = new JCheckBox("Vigenere Cipher");
+		vigenereCheckbox.setBounds(40, 356, 161, 29);
+		panel.add(vigenereCheckbox);
+		
+		JCheckBox hillCheckbox = new JCheckBox("Hill Cipher");
+		hillCheckbox.setBounds(40, 393, 161, 29);
+		panel.add(hillCheckbox);
+		
+		JCheckBox RsaCheckbox = new JCheckBox("RSA Cipher");
+		RsaCheckbox.setBounds(40, 430, 161, 29);
+		panel.add(RsaCheckbox);
+		
+		JCheckBox rootsCheckbox = new JCheckBox("Roots Mod N");
+		rootsCheckbox.setBounds(271, 393, 196, 29);
+		panel.add(rootsCheckbox);
+		
+		JCheckBox inversesCheckbox = new JCheckBox("Inverses Mod N");
+		inversesCheckbox.setBounds(271, 356, 196, 29);
+		panel.add(inversesCheckbox);
+		
+		JCheckBox arithmeticCheckbox = new JCheckBox("Arithmetic Mod N");
+		arithmeticCheckbox.setBounds(271, 319, 196, 29);
+		panel.add(arithmeticCheckbox);
+		
+		JCheckBox indexCheckbox = new JCheckBox("Index of Coincidence");
+		indexCheckbox.setBounds(271, 430, 196, 29);
+		panel.add(indexCheckbox);
+		
+		JCheckBox elGammalCheckbox = new JCheckBox("El Gammal Cipher");
+		elGammalCheckbox.setBounds(40, 467, 161, 29);
+		panel.add(elGammalCheckbox);
+		
+		JCheckBox primitiveCheckbox = new JCheckBox("Primitive Roots");
+		primitiveCheckbox.setBounds(271, 467, 196, 29);
+		panel.add(primitiveCheckbox);
+		
+		JCheckBox multCheckbox = new JCheckBox("Multiplicative Inverses");
+		multCheckbox.setBounds(271, 504, 196, 29);
+		panel.add(multCheckbox);
+		
+		JLabel lblStyle = new JLabel("Style");
+		lblStyle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblStyle.setForeground(Color.WHITE);
+		lblStyle.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		lblStyle.setBounds(717, 206, 139, 35);
+		panel.add(lblStyle);
+		
+		JCheckBox calcCheckbox = new JCheckBox("Calculator");
+		calcCheckbox.setBounds(728, 282, 145, 29);
+		panel.add(calcCheckbox);
+		
+		JCheckBox noCalcCheckbox = new JCheckBox("No Calculator");
+		noCalcCheckbox.setBounds(728, 319, 145, 29);
+		panel.add(noCalcCheckbox);
+	}
+}
---
+++ CryptologyGame/src/MainScreen.java
@@ -0,0 +1,84 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MainScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainScreen window = new MainScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 828, 571);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLACK);
+		panel.setBounds(0, 0, 803, 514);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Converters");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(false);
+				String[] args = new String[0];
+                ConverterWindow.main(args); // Runs the main of Practice Window
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 36));
+		btnNewButton.setBounds(95, 376, 261, 58);
+		panel.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Cryptology Game");
+		lblNewLabel.setBounds(95, 16, 591, 96);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 64));
+		lblNewLabel.setForeground(Color.WHITE);
+		panel.add(lblNewLabel);
+		
+		JButton btnPractice = new JButton("Practice");
+		btnPractice.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				frame.setVisible(false);
+				String[] args = new String[0];
+                PracticeWindow.main(args); // Runs the main of Practice Window
+			}
+		});
+		btnPractice.setFont(new Font("Tahoma", Font.BOLD, 36));
+		btnPractice.setBounds(416, 376, 261, 58);
+		panel.add(btnPractice);
+	}
+}
