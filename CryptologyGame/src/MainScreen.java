8888df3f81841c289cb6e279541b979cb69f0d6c
Zac Tressel <zactressel@yahoo.com>
2020-02-17 11:49:51 -0600

    Edited design

--- CryptologyGame/src/MainScreen.java
+++ CryptologyGame/src/MainScreen.java
@@ -8,6 +8,7 @@ import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
 
 public class MainScreen {
 
@@ -65,8 +66,15 @@ public class MainScreen {
                 PracticeWindow.main(args); // Runs the main of Practice Window
 			}
 		});
-		btnPractice.setFont(new Font("Tahoma", Font.BOLD, 36));
-		btnPractice.setBounds(238, 378, 261, 58);
+		btnPractice.setFont(new Font("Tahoma", Font.BOLD, 48));
+		btnPractice.setBounds(224, 309, 301, 87);
 		panel.add(btnPractice);
+		
+		JLabel lblNewLabel_1 = new JLabel("Made by Zac Tressel");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setForeground(Color.GREEN);
+		lblNewLabel_1.setBounds(265, 478, 215, 20);
+		panel.add(lblNewLabel_1);
 	}
 }
